b60237a37ce66c0fc60badae2c73f97df5bf7d34
khalidsakib <knsakib@gmail.com>
2019-03-02 22:46:28 -0500

    scope of this

--- app.js
+++ app.js
@@ -1,52 +1,33 @@
-const name = 'Zabeer';
-let age = 2;
+const person1 = {
+  name: 'Baby',
+  age: 4,
+  greet: () => {
+    console.log('Hi, I am ' + this.name)
+  }
+};
+person1.greet();
+//Here 'this' is under => function. That is why 'this' refers to global Node runtime scope and not to the object person1.
+//That is why it shows undefimed. Because, arrow function bind 'this' to the container where the object is defined
 
 
-function baby(userName, userAge){
-  return(
-    userName + ' is ' + userAge + ' years ' + 'old'
-  );
-}
-//This is regular/conventinal way of defining function
-console.log(baby(name, age));
+const person2 = {
+  name: 'Zabeer',
+  age: 2,
+  greet: function(){
+    console.log('Hi, I am ' + this.name);
+  }
+};
+person2.greet();
+//Here 'this' refers to the object person2 as greet is function property defined under person2.
 
 
-const baby1 = function (userName, userAge){
-  return(
-    userName + ' is ' + userAge + ' years ' + 'old'
-  );
-}
-//This is an example of annonymous function. Because the variable baby1 is function
-//which does not have any name
-console.log(baby1(name, age));
-
-
-const baby2 = (userName, userAge) => {
-  return(
-    userName + ' is ' + userAge + ' years ' + 'old'
-  );
-}
-//This is an example arrow function. We remove the the word 'function'
-//and place arrow before curly braces. However, other than shortform
-//it has special feature regrading 'this' keyword. Arrow / => function bind the
-//reference of 'this' keyword with the object where 'this' has been called/referenced
-//meaning arrow function will keep the scope of 'this' where the function is defined
-//and not from where it is executed or who executed. Without arrow 'this' refers to the sope of the object
-//where it is executed or who executed.
-console.log(baby2(name, age));
-
-
-const baby3 = (userName, userAge) => userName + ' is ' + userAge + ' years ' + 'old';
-//if arrow function has only one statement we can omit the curly braces and hence if we omit curly braces
-//we need to also remove the return keyword.
-console.log(baby3(name, age));
-
-
-const baby4 = userAge => 'Zabeer' + ' is ' + userAge + ' years ' + 'old';
-//if we have only one argument we can even shorten the function by remove the parenthesis
-console.log(baby4(age));
-
-
-const baby5 = () => 'Zabeer' + ' is ' + '2' + ' years ' + 'old';
-//However if we do not have any argument we have to use ()
-console.log(baby5());
+const person3 = {
+  name: 'Aysel',
+  age: 3,
+  greet(){
+    console.log('Hi, I am ' + this.name);
+  }
+};
+person3.greet();
+//Here greet() is a method of the object 'person3'. It is ot defined under any arrow function. So, 'this' refers to the
+//object where the method is executing, which is person3
