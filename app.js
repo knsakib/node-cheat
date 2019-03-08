8e5b6023e21ee3e51dd6ea0f3c2433e02782a8f3
khalidsakib <knsakib@gmail.com>
2019-03-07 21:09:57 -0500

    Routing-Requests

--- app.js
+++ app.js
@@ -1,14 +1,29 @@
 const http=require('http');
+const fs = require('fs');
 
 const server = http.createServer((req, res)=>{
-  console.log(req.url, req.method, req.headers);
+  const url=req.url;
+  const method=req.method;
+  //we are reading the infoirmation of the request and route the request based on that
+  if (url==='/'){
+  //'===' means if both type(here it is string) and value matched
   res.setHeader('Content-Type', 'text/html');
   //we tell the server what is the content type
   res.write('<html>');
-  res.write('<h1>Node Server</h1>');
+  res.write('<head><title>Enter Message</title></head>');
+  res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>');
+  //GET request is default and automatic. But for POST request, we/client should send the request to the server using form or javascipt. It will also sends some other
+  //data with input text, for example name(here it is message)
   res.write('</html>');
-  res.end();
-  //respond ends. We can write anything in the response after this. After this it will comeback to the server from the browser
+  return res.end();
+  //it will quite the function after here as it is a return statement. because we dont want to move next
+}
+res.setHeader('Content-Type', 'text/html');
+res.write('<html>');
+res.write('<head><title>After Posting the Message</title></head>');
+res.write('<body><h1>Page after receiving the POST request!</h1></body>');
+res.write('</html>');
+res.end();
 });
 
 server.listen(3000);
