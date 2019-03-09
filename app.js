0e8e4217bf068094b71f96b7cd326aab5b71c3cb
khalidsakib <knsakib@gmail.com>
2019-03-09 12:02:49 -0500

    summery

--- app.js
+++ app.js
@@ -2,11 +2,6 @@ const http=require('http');
 
 const routes=require('./routes');
 
-const server = http.createServer(routes.handler);
-//we created the routes module and we imported here.
-//Then we pass it to http.createServer
-//We just reference the function we are not executing here, telling that please execute for incoming request
-
-console.log(routes.someText)
+const server = http.createServer(routes);
 
 server.listen(3000);
