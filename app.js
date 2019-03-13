0eae99bcb4cff23ea244b641d1fa295ae9da000e
khalidsakib <knsakib@gmail.com>
2019-03-12 22:26:42 -0400

    Lesson-62-Express-Middleware-Next-or-End

--- app.js
+++ app.js
@@ -1,30 +1,23 @@
 const express = require('express');
 
 const app = express();
-//express execute a function that is initializing 'app' onject 
-//app object is also a request handler 
-//express will give a middleware functionality that we can plug into express
-//middleware means an incoming request is automatically funnelled through some series of functions until at the end we send the resposne  
-
+  
 app.use('/', (req, res, next)=>{
-    console.log('This middleware always runs');
+    console.log('This is the First Middleware middleware');
     next();
 });
 
-app.use('/add-product', (req, res, next)=>{
-    console.log('In the middleware');
-    res.send('<h1>Product Blah!</h1>')
-});
-
 app.use('/', (req, res, next)=>{
-    console.log('In another middleware');
-    res.send('<h1>Hello Blah!</h1>')
+    console.log('This is the Second Middleware middleware');
+    next();
 });
-// We should put as a last middle for routing as this is for '/'
 
-// const http=require('http');
-// const server = http.createServer(app);
-// server.listen(3000);
-//all the three lines above equal to 
+app.use('/users', (req, res, next)=>{
+    res.send('<h1>User Section</h1>')
+});
+
+app.use('/', (req, res)=>{
+    res.send('<h1>Main Section</h1>')
+});
 
 app.listen(3000);
