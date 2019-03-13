3af3f621ebdd84a801b1d7dc07c9d11d9bea81ba
khalidsakib <knsakib@gmail.com>
2019-03-12 22:07:17 -0400

    Express Routing

--- app.js
+++ app.js
@@ -1,28 +1,30 @@
-const http=require('http');
-
 const express = require('express');
 
 const app = express();
 //express execute a function that is initializing 'app' onject 
-
-const server = http.createServer(app);
 //app object is also a request handler 
 //express will give a middleware functionality that we can plug into express
 //middleware means an incoming request is automatically funnelled through some series of functions until at the end we send the resposne  
 
-app.use((req, res, next)=>{
-    console.log('In the middleware');
+app.use('/', (req, res, next)=>{
+    console.log('This middleware always runs');
     next();
-    //next will alow the request to travel to next middleware
-    //middleware will flow through top to bottom
 });
 
-app.use((req, res, next)=>{
+app.use('/add-product', (req, res, next)=>{
+    console.log('In the middleware');
+    res.send('<h1>Product Blah!</h1>')
+});
+
+app.use('/', (req, res, next)=>{
     console.log('In another middleware');
-    res.send("<h1>Blah!</h1>")
-    //send is utility function comes from express. By this we can send a request body 
-    //By deafult text tyoe is html 
-    
+    res.send('<h1>Hello Blah!</h1>')
 });
+// We should put as a last middle for routing as this is for '/'
 
-server.listen(3000);
+// const http=require('http');
+// const server = http.createServer(app);
+// server.listen(3000);
+//all the three lines above equal to 
+
+app.listen(3000);
