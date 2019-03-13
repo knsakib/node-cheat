8f979c2aee62e0734ca18236950016e8878889be
khalidsakib <knsakib@gmail.com>
2019-03-13 00:03:06 -0400

    Lesson-63-Parsing-Incoming-Request-by-body-parser

--- app.js
+++ app.js
@@ -1,19 +1,20 @@
 const express = require('express');
+const bodyParser=require('body-parser');
 
 const app = express();
+
+app.use(bodyParser.urlencoded({extended: false}));
+//this body Parser middleware automatically call next, so that it will go to all the next url handler middlewares after request parsing
+//This parser will parse only post request using form submit
   
-app.use('/', (req, res, next)=>{
-    console.log('This is the First Middleware middleware');
-    next();
+app.use('/add-product', (req, res, next)=>{
+    res.send('<form action="/product" method="POST"><input type="text" name="title" /><button type="submit">Add Product</button></form>')
 });
 
-app.use('/', (req, res, next)=>{
-    console.log('This is the Second Middleware middleware');
-    next();
-});
-
-app.use('/users', (req, res, next)=>{
-    res.send('<h1>User Section</h1>')
+app.use('/product', (req, res, next)=>{
+    console.log(req.body);
+    //it will automatically parse the body. To do this, we need to regsiter a parser called body perser. It is a 3rd party library middleware which we register @line6 
+    res.redirect('/')
 });
 
 app.use('/', (req, res)=>{
