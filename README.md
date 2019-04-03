42b027e432da0043d38c4c44edcda82c2b1dad92
knsakib <knsakib@gmail.com>
2019-04-02 21:33:08 -0400

    Lesson-90-Ejs-Render-and-Partials

--- README.md
+++ README.md
@@ -17,20 +17,10 @@ We need pass keyname as second parameter to send dynamic
 data and render it to template. 
 ```
 
-### handlebars Philosophy
+### EJS Rendering
 ```
-Te cannot put any logic we can just check the value of the logic we put here. 
-This is in a sense good because in that case we are forced to put logic
-in our node.js where it should be and template engine will be used only
-for rendering purpose.  
-```
-
-
-### Layout in handlebars
-```
-We can use common layout in Handlebars. But we need specify main layour name in
-app.js. However, for conditional render in common layout, the logic should be 
-in node side. We will check the value that is passed in main layout and 
-check the value tru e or false to render different layout in different 
-condition. 
+When are importing partials we can use <%= %> to render it as text
+instead of html to avoid CSRF attack. With <%- %> it will render it 
+as HTML. If there is any variables in the partials it will work with
+<%- %>
 ```
--- app.js
+++ app.js
@@ -1,18 +1,22 @@
 const express = require('express');
 const bodyParser=require('body-parser');
 const path = require('path');
-const expressHbs=require('express-handlebars');
+// const expressHbs=require('express-handlebars');
 //Handlebars is not automatically register or included in express
 //we need to import it manually  
 
 const app = express();
 
 
-app.engine('hbs', expressHbs({layoutsDir: 'views/layouts/', defaultLayout: 'main-layout', extname:'hbs'}));
+// app.engine('hbs', expressHbs({layoutsDir: 'views/layouts/', defaultLayout: 'main-layout', extname:'hbs'}));
 //Handlebars is not automatically register or included in express
 //we need to import it manually  
-app.set('view engine', 'hbs');
+// app.set('view engine', 'hbs');
 //layoutsDir and main-layout is to specify if location and files are different than default 
+
+//ejs
+app.set('view engine', 'ejs');
+
 
 //pug:
 //app.set('view engine', 'pug');
@@ -49,7 +53,7 @@ app.use(shopRoutes);
 
 app.use((req, res, next)=>{
     // res.status(404).sendFile(path.join(rootDir, 'views', 'page-not-found.html'));
-    res.status(404).render('page-not-found', {pageTitle: 'Page Not Found'});
+    res.status(404).render('page-not-found', {pageTitle: 'Page Not Found', path: ''});
 })
 //Deafult path is '/', so we need not mention. It will work all the not defined path 
 
